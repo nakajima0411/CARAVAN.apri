7d04644edfc7375547e6e80c58cd0a1eea4c8647
nakajima0411 <sphere.system0411@gmail.com>
2018-06-12 14:27:50 +0900

    Finish 4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,24 +1,28 @@
 class BlogsController < ApplicationController
   def index
+  	  # 記事を全件取得
+  	  @blogs = Blog.all
   end
 
   def show
+  	  @blog = Blog.find(params[:id])
   end
 
   def new
-  	@blog = Blog.new
+   	  @blog = Blog.new
   end
+
   def create
-  	blog = Blog.new(blog_params)
-  	blog.save
-  	redirect_to blogs_path
+  	  blog = Blog.new(blog_params)
+  	  blog.save
+  	  redirect_to blogs_path
   end
 
   def edit
   end
 
   private
-    def blog_paramas
+    def blog_params
     	params.require(:blog).permit(:title, :category, :body)
     end
 end
